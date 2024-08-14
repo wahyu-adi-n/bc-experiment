7cda10ebb84a192ab1f5056f2d9ef7166e69dc6e
Wahyu Adi Nugroho <wahyu.adinugroho@delameta.com>
2024-08-14 08:10:46 +0700

    add gradcam feature for XAI purposes

---
+++ run/cam.sh
@@ -0,0 +1,4 @@
+python gradcam.py \
+    --image-path /home/delameta/Research/BC/experiment/dataset/BreaKHis_v1/histology_slides/breast/benign/SOB/adenosis/SOB_B_A_14-22549AB/40X/SOB_B_A-14-22549AB-40-001.png \
+    --device cuda:0 \
+    --output-dir output/Select/Test-DenseNet121-bin-relu
